22e4f4039297f8488b08c9713d79cddae96a9a23
Brayan M <brihanm.work@gmail.com>
2025-04-06 20:39:08 -0500

    Add FooterComp in app.comp.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavBarComponent, ServicesComponent],
+  imports: [RouterOutlet, HomeComponent, NavBarComponent, ServicesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
