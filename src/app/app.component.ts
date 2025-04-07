5dc45149e156e0f2a37c8f0b4573de2828c6e407
Brayan M <brihanm.work@gmail.com>
2025-04-06 20:46:30 -0500

    Add comp and info in AboutUs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ServicesComponent } from './services/services.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavBarComponent, ServicesComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, NavBarComponent, ServicesComponent, FooterComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
