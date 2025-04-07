7fb5131ef670171ca484a7ed74434ff551bf4666
Brayan M <brihanm.work@gmail.com>
2025-04-06 22:18:15 -0500

    Route about-us and edit home-comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ContactFormWebComponent } from './contact-form-web/contact-form-web.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     // ... rutas existentes
@@ -13,5 +14,10 @@ export const routes: Routes = [
         path: 'Contact',
         component: ContactFormWebComponent,
         title: 'NexAur Technologies - Contacto'
+    },
+    {
+        path: '**',
+        component: AboutUsComponent,
+        title: 'NexAur Technologies - Acerca de nosotros'
     }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ServicesComponent } from '../services/services.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ServicesComponent],
+  imports: [ServicesComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
